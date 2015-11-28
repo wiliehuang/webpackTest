ca0231f5240a717ec195f3a36b360260cf4e7740
Willie Huang <williehuang@d192-075-247-113.wireless.sfu.ca>
2015-11-28 10:11:35 -0800

    update

--- src/components/App11.jsx
+++ src/components/App11.jsx
@@ -7,13 +7,55 @@ import AppBar from 'material-ui/lib/app-bar';
 import BarChart from 'react-d3-components/lib/BarChart';
 import BarChart1 from 'react-d3/barchart/BarChart';
 
+// import Colors from 'material-ui/lib/styles/colors';
+// import ThemeManager from 'material-ui/lib/styles/theme-manager';
 
+// import ThemeManager from 'material-ui/lib/styles/theme-manager';
+// import Colors from 'material-ui/lib/styles/colors';
+// import AppBar from 'material-ui/lib/app-bar';
+// import rawTheme1 from 'material-ui/lib/styles/themes/light-theme';
+//import ThemeDecorator from 'material-ui/lib/styles/theme-decorator';
+
+// const muiTheme = ThemeManager.modifyRawThemePalatte(ThemeManager.getMuiTheme(rawTheme1),{
+//   primary1Color: Colors.blue500,
+//   primary2Color: Colors.blue700,
+//   primary3Color: Colors.blue100,
+//   accent1Color: Colors.pink400
+// });
+
+var data = [
+    {
+    label: 'TA',
+    values: [{x: 'teamA', y: 10}, {x: 'teamB', y: 4}, {x: 'teamC', y: 3}]
+    },
+    {
+    label: 'TB',
+    values: [{x: 'teamA', y: 6}, {x: 'teamB', y: 8}, {x: 'teamC', y: 5}]
+    },
+    {
+    label: 'TC',
+    values: [{x: 'teamA', y: 6}, {x: 'teamB', y: 8}, {x: 'teamC', y: 5}]
+    }
+];
 
 class App extends React.Component {
   constructor(){
     super();
 
   }
+
+  // static childContextTypes = {
+  //   muiTheme: React.PropTypes.object
+  // }
+  //
+  // getChildContext(){
+  //   return {
+  //     muiTheme: ThemeManager.getMuiTheme(MyRawTheme)
+  //   }
+  // }
+  //var BarChart = Dthree.BarChart;
+
+
 
   render(){
     return(
@@ -41,7 +83,7 @@ class App extends React.Component {
           data={data}
           width={400}
           height={400}
-          fill='#bd6c35'
+          fill={'#000000'}
           margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
 
         <BarChart1
